60f2aaf944254da4ebe06beb34dde9d5da43fcaf
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-02 10:17:06 -0300

    structure set

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+//* components
 import { HeaderComponent } from './components/header/header.component';
-import { ButtonDeleteAllComponent } from './components/button-delete-all/button-delete-all.component';
-import { InputAddItemsComponent } from './components/input-add-items/input-add-items.component';
-import { ListComponent } from './components/list/list.component';
 import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
 import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
+
+//* pages
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ButtonDeleteAllComponent,
-    InputAddItemsComponent,
-    ListComponent,
     TodoButtonDeleteAllComponent,
     TodoInputAddItemsComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItemsComponent,
+    TodoListComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule

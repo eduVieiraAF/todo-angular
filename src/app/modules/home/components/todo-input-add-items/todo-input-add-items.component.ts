ee7be3872372fe9f49bc018268fcda02e0d8fd85
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-09-07 15:20:50 -0300

    adding task and emitting to components

--- src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
+++ src/app/modules/home/components/todo-input-add-items/todo-input-add-items.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-items',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-input-add-items.component.scss']
 })
 export class TodoInputAddItemsComponent {
+  @Output() public emitItemTaskList = new EventEmitter();
+  public addItemTaskList: string = '';
+
+  public submitItemTaskList() {
+    // console.log(this.addItemTaskList)
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if (this.addItemTaskList ) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = '';
+    }
+  }
 
 }
